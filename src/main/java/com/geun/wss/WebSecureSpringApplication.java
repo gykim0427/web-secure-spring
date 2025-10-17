b15ee3d86c31e994055fc5151cad518dbcea3dd1
gykim <gykim0427@naver.com>
2025-10-17 15:13:57 +0900

    first project commit

---
+++ src/main/java/com/geun/wss/WebSecureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.geun.wss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geun/wss/WebSecureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geun.wss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
