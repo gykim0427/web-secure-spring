f751787860f6a6f44ed494bd9d154aa5f84c8a0d
gykim <gykim0427@naver.com>
2025-10-23 17:31:25 +0900

    login page -> main page controller setting

--- src/main/java/com/geun/wss/login/controller/LoginController.java
+++ src/main/java/com/geun/wss/login/controller/LoginController.java
@@ -1,7 +1,7 @@
 package com.geun.wss.login.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
-	@PostMapping(value = { "" }, produces = { "application/json" })
+	@GetMapping(value = { "/" }, produces = { "application/json" })
 	public ModelAndView empam0210m(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
 		ModelAndView view = new ModelAndView();
 		view.setViewName("views/login");
